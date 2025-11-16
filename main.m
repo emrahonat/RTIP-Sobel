dacd45a5e59cca008f6874b8558be616f628eff7
emrah onat <eonat87@yahoo.com>
2025-11-17 00:40:58 +0100

    first commit

---
+++ main.m
@@ -0,0 +1,12 @@
+%% Real-Time FPGA Implementation of Adaptive and Improved Sobel Edge Detection Algorithm for Image and Video Processing
+
+% 
+% 0 - Matlab Code for Sobel Edge Detection
+% 1 - Matlab Code for Improved Sobel Edge Detection
+% 2 - Matlab Code for Adaptive and Improved Sobel Edge Detection
+%
+% Dr. Emrah Onat
+% 17.11.2025
+% 
+
+%% Main Code 
