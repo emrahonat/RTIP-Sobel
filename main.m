bcc4ffc48b3da562d70d2f70af08593216087660
emrah onat <eonat87@yahoo.com>
2025-11-18 00:08:18 +0100

    commit

--- main.m
+++ main.m
@@ -5,6 +5,9 @@
 % 1 - Matlab Code for Improved Sobel Edge Detection
 % 2 - Matlab Code for Adaptive and Improved Sobel Edge Detection
 %
+% Video Frame 1
+% Video Frame 2
+% 
 % Dr. Emrah Onat
 % 17.11.2025
 % 
